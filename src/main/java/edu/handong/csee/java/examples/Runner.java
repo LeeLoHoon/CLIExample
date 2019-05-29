e87fa7c047a0cf43f512e8df87e41e653ff38332
leelohoon <21600558@handong.edu>
2019-05-30 01:23:08 +0900

    save

--- src/main/java/edu/handong/csee/java/examples/Runner.java
+++ src/main/java/edu/handong/csee/java/examples/Runner.java
@@ -1,5 +1,7 @@
 package edu.handong.csee.java.examples;
 
+import java.io.File;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -10,6 +12,7 @@ import org.apache.commons.cli.Options;
 public class Runner {
 	
 	String path;
+	boolean fullpath;
 	boolean verbose;
 	boolean help;
 
@@ -32,16 +35,34 @@ public class Runner {
 			// path is required (necessary) data so no need to have a branch.
 			System.out.println("You provided \"" + path + "\" as the value of the option p");
 			
+			
 			// TODO show the number of files in the path
+			File file = new File(path);
+			File parentFile = new File(file.getAbsoluteFile().getParent());
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.getName());
+			
+			System.out.println(parentFile.listFiles().length);
 			
 			if(verbose) {
 				
 				// TODO list all files in the path
-				
-				System.out.println("Your program is terminated. (This message is shown because you turned on -v option!");
+				File[] files = parentFile.listFiles();
+				if(fullpath) {
+					for(File k : files) {
+						System.out.println(k);
+					}
+					System.out.println("Your program is terminated. (This message is shown because you turned on -v option!");
+				}
+				else {
+					for(File k : files) {
+						System.out.println(k.getName());
+					}
+				}
 			}
 		}
 	}
+
 
 	private boolean parseOptions(Options options, String[] args) {
 		CommandLineParser parser = new DefaultParser();
@@ -50,9 +71,11 @@ public class Runner {
 
 			CommandLine cmd = parser.parse(options, args);
 
-			path = cmd.getOptionValue("p");
-			verbose = cmd.hasOption("v");
-			help = cmd.hasOption("h");
+			path = cmd.getOptionValue("p");  // p 뒤의 argument 가져오기
+			fullpath = cmd.hasOption("f");
+			verbose = cmd.hasOption("v"); //boolean 있으면 true
+			help = cmd.hasOption("h"); //boolean
+			
 
 		} catch (Exception e) {
 			printHelp(options);
@@ -68,10 +91,15 @@ public class Runner {
 
 		// add options by using OptionBuilder
 		options.addOption(Option.builder("p").longOpt("path")
-				.desc("Set a path of a directory or a file to display")
-				.hasArg()
+				.desc("Set a path of a directory or a file to display") //설명
+				.hasArg() //이 옵션은 arg가 필요
 				.argName("Path name to display")
-				.required()
+				.required() //필수 요소& 없으면 error
+				.build());
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("f").longOpt("fullpath")
+				.desc("Display fullpath of all files")
+				.argName("FullPath name of files in directory to display")
 				.build());
 
 		// add options by using OptionBuilder
@@ -86,7 +114,11 @@ public class Runner {
 		options.addOption(Option.builder("h").longOpt("help")
 		        .desc("Help")
 		        .build());
+		
 
+				
+		
+		
 		return options;
 	}
 	
